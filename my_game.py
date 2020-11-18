a3f4922dd2d8149a176f6c4c2c0e8f7fe9ae5302
Fabian Luettel <fabianluettel@web.de>
2020-11-18 19:24:40 +0100

    basic stuff

---
+++ my_game.py
@@ -0,0 +1,36 @@
+#STEP(1): Import libraries
+import gym
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#STEP(2): xxxxxxxxx
+
+
+env = gym.make('CarRacing-v0') #other famous game is CartPole-v0
+duration_of_sim = 1000; #duration of simulation in ms
+n_iterations = 3; #number of iterations
+for i_episode in range(n_iterations):
+    observation = env.reset()
+    for t in range(duration_of_sim):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action) # take a random action; step delivers: observation, reward, done and info
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+
+
+#There are 5 potential actions:
+#1: Forward/Accelerate
+#2: Backward/Decelerate/Brake
+#3: Steer to left
+#4: Steer to right
+#5: do nothing
+
+
+
+#input("Press enter to exit this program.")
